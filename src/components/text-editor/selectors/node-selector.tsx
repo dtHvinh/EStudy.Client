9208fed9649e8bd984f4d5df88024e8c126ed359
dtHvinh <dangthehienvinh662@gmail.com>
2025-06-20 07:43:26 +0700

    fix vercel err

--- src/components/text-editor/selectors/node-selector.tsx
+++ src/components/text-editor/selectors/node-selector.tsx
@@ -29,69 +29,69 @@ const items: SelectorItem[] = [
     name: "Text",
     icon: TextIcon,
     command: (editor) =>
-      editor.chain().focus().toggleNode("paragraph", "paragraph").run(),
+      editor!.chain().focus().toggleNode("paragraph", "paragraph").run(),
     // I feel like there has to be a more efficient way to do this – feel free to PR if you know how!
     isActive: (editor) =>
-      editor.isActive("paragraph") &&
-      !editor.isActive("bulletList") &&
-      !editor.isActive("orderedList"),
+      editor!.isActive("paragraph") &&
+      !editor!.isActive("bulletList") &&
+      !editor!.isActive("orderedList"),
   },
   {
     name: "Heading 1",
     icon: Heading1,
     command: (editor) =>
-      editor.chain().focus().toggleHeading({ level: 1 }).run(),
-    isActive: (editor) => editor.isActive("heading", { level: 1 }),
+      editor!.chain().focus().toggleHeading({ level: 1 }).run(),
+    isActive: (editor) => editor!.isActive("heading", { level: 1 }),
   },
   {
     name: "Heading 2",
     icon: Heading2,
     command: (editor) =>
-      editor.chain().focus().toggleHeading({ level: 2 }).run(),
-    isActive: (editor) => editor.isActive("heading", { level: 2 }),
+      editor!.chain().focus().toggleHeading({ level: 2 }).run(),
+    isActive: (editor) => editor!.isActive("heading", { level: 2 }),
   },
   {
     name: "Heading 3",
     icon: Heading3,
     command: (editor) =>
-      editor.chain().focus().toggleHeading({ level: 3 }).run(),
-    isActive: (editor) => editor.isActive("heading", { level: 3 }),
+      editor!.chain().focus().toggleHeading({ level: 3 }).run(),
+    isActive: (editor) => editor!.isActive("heading", { level: 3 }),
   },
   {
     name: "To-do List",
     icon: CheckSquare,
-    command: (editor) => editor.chain().focus().toggleTaskList().run(),
-    isActive: (editor) => editor.isActive("taskItem"),
+    command: (editor) => editor!.chain().focus().toggleTaskList().run(),
+    isActive: (editor) => editor!.isActive("taskItem"),
   },
   {
     name: "Bullet List",
     icon: ListOrdered,
-    command: (editor) => editor.chain().focus().toggleBulletList().run(),
-    isActive: (editor) => editor.isActive("bulletList"),
+    command: (editor) => editor!.chain().focus().toggleBulletList().run(),
+    isActive: (editor) => editor!.isActive("bulletList"),
   },
   {
     name: "Numbered List",
     icon: ListOrdered,
-    command: (editor) => editor.chain().focus().toggleOrderedList().run(),
-    isActive: (editor) => editor.isActive("orderedList"),
+    command: (editor) => editor!.chain().focus().toggleOrderedList().run(),
+    isActive: (editor) => editor!.isActive("orderedList"),
   },
   {
     name: "Quote",
     icon: TextQuote,
     command: (editor) =>
-      editor
+      editor!
         .chain()
         .focus()
         .toggleNode("paragraph", "paragraph")
         .toggleBlockquote()
         .run(),
-    isActive: (editor) => editor.isActive("blockquote"),
+    isActive: (editor) => editor!.isActive("blockquote"),
   },
   {
     name: "Code",
     icon: Code,
-    command: (editor) => editor.chain().focus().toggleCodeBlock().run(),
-    isActive: (editor) => editor.isActive("codeBlock"),
+    command: (editor) => editor!.chain().focus().toggleCodeBlock().run(),
+    isActive: (editor) => editor!.isActive("codeBlock"),
   },
 ];
 interface NodeSelectorProps {
